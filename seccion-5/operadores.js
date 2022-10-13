ec2ed93227e4950f3fdb15f109ca1caa6e83cb97
nxhuel <nahueltisera03@gmail.com>
2022-10-12 21:05:29 -0300

    agregue los ejercicios de la seccion 5

---
+++ seccion-5/operadores.js
@@ -0,0 +1,14 @@
+var valores = [true, 5, false, "hola", "adios", 2];
+
+var resultados = (valores[3]) > (valores [4]);
+
+var resultados = valor1 || valor2;
+var resultados = valor1 &&  valor2;
+
+var num1 = (valores [1]);
+var num2 = (valores [5]);
+var suma = num1 + num2;
+var resta = num1 - num2;
+var multiplicacion = num1 * num2;
+var division = num1 / num2;
+var modulo = num1 % num2;
